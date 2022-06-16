fcef7f83a5378e4dc9cf42db61492aae3183a903
Maiara Reinaldo <maiara.reinaldo@fundingcircle.com>
2022-06-16 17:02:06 +0100

    Build files

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,7 @@
+export default [
+  require('/Users/maiara.reinaldo/Workspace/test-pages/node_modules/infima/dist/css/default/default.css'),
+  require('/Users/maiara.reinaldo/Workspace/test-pages/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
+  require('/Users/maiara.reinaldo/Workspace/test-pages/node_modules/@docusaurus/theme-classic/lib/admonitions.css'),
+  require('/Users/maiara.reinaldo/Workspace/test-pages/node_modules/@docusaurus/theme-classic/lib/nprogress'),
+  require('/Users/maiara.reinaldo/Workspace/test-pages/src/css/custom.css'),
+];
